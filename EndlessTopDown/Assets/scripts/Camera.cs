27b31ac442b331396d12b100e991315fe178cebb
Matthieu40 <mlegagneur@smu.edu>
2021-02-05 01:03:31 -0600

    Camera and movement configured. Need to fix movement in the player script.

---
+++ EndlessTopDown/Assets/scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    //variables
+    public Transform player;
+    public float smooth = 0.3f; //camera smoothness
+
+    public float height;
+
+    private Vector3 velocity = Vector3.zero; //sets camera velocity to 0 when afk
+
+   //Methods
+   void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.z = player.position.z - 4f; //how far the camera is behind the player (without 7f, camera is above player.)
+        pos.y = player.position.y + height; //The + height lets the camera height be manipulated
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+    }
+
+}
---
+++ EndlessTopDown/Assets/scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //variables
+    public float movementSpeed; //player movement speed
+    public GameObject camera; 
+
+
+    //Methods
+    void update()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position); //plane that tracks camera position
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition); //player always faces direction of cursor
+        float hitDist = 0.0f;
+
+        if(playerPlane.Raycast(ray, out hitDist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+        //player Movement
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        
+    }
+
+}
