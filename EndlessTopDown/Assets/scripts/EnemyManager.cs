2d4eda6ee6f63acc21b13f3e5367a8a5e7ac69b3
Matthieu40 <mlegagneur@smu.edu>
2021-04-15 21:17:23 -0500

    Prototype8

---
+++ EndlessTopDown/Assets/scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform[] Spawnpoint;
+    public GameObject EnemyPrefab;
+    public int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNewEnemy();
+    }
+
+    // Update is called once per frame
+    void SpawnNewEnemy()
+        {
+        while (enemyCount < 10)
+        {
+            int randomNumber = Mathf.RoundToInt(Random.Range(0f, Spawnpoint.Length - 1));
+            Instantiate(EnemyPrefab, Spawnpoint[randomNumber].transform.position, Quaternion.identity);
+        }    }
+}
